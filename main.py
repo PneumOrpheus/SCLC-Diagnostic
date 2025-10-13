c7ae37f6d7b51b385247bab94df04402aad60147
Hans Stemshaug <hansstem@sintef-lunge-home.novalocal>
2025-10-13 13:44:53 +0200

    added requirements.txt

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pydicom
 
 dicom_file_path = '../work/BIG_LUNGE/CT_images/1/DICOM/000066EC/AAC37262/AA143FD3/00007DE1/EE000524'
 try:
-    ds = pydicom.filereader.dcmread(dicom_file_path)
+    ds = pydicom.dcmread(dicom_file_path)
     print(ds)
 except Exception as error:
     print("Error reading DICOM file:", error)
