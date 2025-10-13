5be9c4797d5d2c7a6a4654ea614510b18d751ede
Hans Stemshaug <hansstem@sintef-lunge-home.novalocal>
2025-10-13 14:18:07 +0200

    added converting to NIFTI logic

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pydicom
+import dicom2nifti
+import os
+import dicom2nifti.settings as settings
 
-dicom_file_path = '../work/BIG_LUNGE/CT_images/1/DICOM/000066EC/AAC37262/AA143FD3/00007DE1/EE000524'
+dicom_file_path = '../work/BIG_LUNGE/CT_images/1/DICOM/000066EC/AAC37262/AA143FD3/00007DE1/'
+nifti_output_path = './NIFTI_data'
+
 try:
-    ds = pydicom.dcmread(dicom_file_path)
-    print(ds)
+    os.makedirs(nifti_output_path, exist_ok=True)
+
+    dicom2nifti.convert_directory(
+        dicom_file_path,
+        nifti_output_path,
+        compression=True,
+        reorient=True
+    )
 except Exception as error:
     print("Error reading DICOM file:", error)
