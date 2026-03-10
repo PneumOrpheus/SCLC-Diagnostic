5d0f1ebfb206fdb1d54054e9edb3b462e9af8666
Rafael Oversand <rafael.oversand@outlook.com>
2026-03-10 07:31:36 +0000

    prevent deprecation issues

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,3 @@ from .build import build_model
 from .utils import load_pretrained
 from .swin_transformer import SwinTransformer
 from .swin_transformer_v2 import SwinTransformerV2
-from .swin_transformer_moe import SwinTransformerMoE
-from .swin_mlp import SwinMLP
-from .simmim import build_simmim
